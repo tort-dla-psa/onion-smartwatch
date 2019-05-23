d1930adf70f95122dead0137b3ac4ff9e8352175
tort_dla_psa <alexeev-eugene@outlook.com>
2019-05-23 19:57:01 +0300

    now calc ui is created with ui_stacks

--- apps/calc/main.cpp
+++ apps/calc/main.cpp
@@ -5,24 +5,28 @@
 #include "button.h"
 #include "watchlib.h"
 #include "binform.h"
+#include "image.h"
+#include "stack.h"
 
 const int w = 128, h = 64;
 template<typename T>
 using sptr = std::shared_ptr<T>;
+using namespace binforms;
+using namespace watches;
 
 class myform:public binform{
 	std::string first, second;
 	bool enter_second;
-	void move_after_right(const sptr<button> &el, sptr<button> el2){
-		el2->move(el->get_x()+el->get_w(), el->get_y());
-	}
-	void move_after_down(const sptr<button> &el, sptr<button> el2){
-		el2->move(el->get_x(), el->get_y()+el->get_h());
-	}
 	sptr<label> lbl;
 	sptr<std::function<void(void)>> f;
 	void update_label(){
-		lbl->
+	//	lbl->
+	}
+	std::shared_ptr<button> mk_btn(std::vector<std::shared_ptr<button>> &vec,
+		const std::string &lbl)
+	{
+		return vec.emplace_back(new button(lbl));
+	}
 public:
 	myform():binform(w,h){
 		f = nullptr;
@@ -34,51 +38,80 @@ public:
 		buttons.reserve(15);
 
 		lbl = std::make_shared<label>(first);
-		sptr<button> b9 = buttons.emplace_back(new button("9"));
-		sptr<button> b8 = buttons.emplace_back(new button("8"));
-		sptr<button> b7 = buttons.emplace_back(new button("7"));
-		sptr<button> b6 = buttons.emplace_back(new button("6"));
-		sptr<button> b5 = buttons.emplace_back(new button("5"));
-		sptr<button> b4 = buttons.emplace_back(new button("4"));
-		sptr<button> b3 = buttons.emplace_back(new button("3"));
-		sptr<button> b2 = buttons.emplace_back(new button("2"));
-		sptr<button> b1 = buttons.emplace_back(new button("1"));
-		sptr<button> b0 = buttons.emplace_back(new button("0"));
-		sptr<button> b_dot = buttons.emplace_back(new button("."));
-		sptr<button> b_plus = actions.emplace_back(new button("+"));
-		sptr<button> b_minus = actions.emplace_back(new button("-"));
-		sptr<button> b_mul = actions.emplace_back(new button("*"));
-		sptr<button> b_div = actions.emplace_back(new button("/"));
-		sptr<button> b_eq = actions.emplace_back(new button("="));
+		auto b9 = mk_btn(buttons,("9"));
+		auto b8 = mk_btn(buttons,"8");
+		auto b7 = mk_btn(buttons,"7");
+		auto b6 = mk_btn(buttons,"6");
+		auto b5 = mk_btn(buttons,"5");
+		auto b4 = mk_btn(buttons,"4");
+		auto b3 = mk_btn(buttons,"3");
+		auto b2 = mk_btn(buttons,"2");
+		auto b1 = mk_btn(buttons,"1");
+		auto b0 = mk_btn(buttons,"0");
+		auto b_dot = mk_btn(buttons,".");
+		auto b_plus = mk_btn(actions,"+");
+		auto b_minus = mk_btn(actions,"-");
+		auto b_mul = mk_btn(actions,"*");
+		auto b_div = mk_btn(actions,"/");
+		auto b_eq = mk_btn(actions,"=");
 
 		lbl->move(0,0);
 		b9->move(0, lbl->get_h());
-		move_after_right(b9, b8);
-		move_after_right(b8, b7);
-		move_after_down(b9, b6);
-		move_after_right(b6, b5);
-		move_after_right(b5, b4);
-		move_after_down(b6, b3);
-		move_after_right(b3, b2);
-		move_after_right(b2, b1);
-		move_after_down(b3, b0);
-		move_after_right(b0, b_dot);
-		move_after_right(b_dot, b_eq);
-		move_after_right(b7, b_plus);
-		move_after_down(b_plus, b_minus);
-		move_after_down(b_minus, b_mul);
-		move_after_down(b_mul, b_div);
+		auto st_v = std::make_shared<v_stack>();
+		auto st_h = std::make_shared<h_stack>();
+		st_h->add_element(b9);
+		st_h->add_element(b8);
+		st_h->add_element(b7);
+		st_h->update();
+		st_v->add_element(std::move(st_h));
+		st_v->update();
+
+		st_h = std::make_shared<h_stack>();
+		st_h->add_element(b6);
+		st_h->add_element(b5);
+		st_h->add_element(b4);
+		st_h->update();
+		st_v->add_element(std::move(st_h));
+		st_v->update();
+
+		st_h = std::make_shared<h_stack>();
+		st_h->add_element(b3);
+		st_h->add_element(b2);
+		st_h->add_element(b1);
+		st_h->update();
+		st_v->add_element(std::move(st_h));
+		st_v->update();
+
+		st_h = std::make_shared<h_stack>();
+		st_h->add_element(b0);
+		st_h->add_element(b_dot);
+		st_h->add_element(b_eq);
+		st_h->update();
+		st_v->add_element(std::move(st_h));
+		st_v->update();
+
+		st_h = std::make_shared<h_stack>();
+		auto st_funcs = std::make_shared<v_stack>();
+		st_funcs->add_element(b_plus);
+		st_funcs->add_element(b_minus);
+		st_funcs->add_element(b_mul);
+		st_funcs->add_element(b_div);
+		st_funcs->update();
+		st_h->add_element(st_v);
+		st_h->add_element(st_funcs);
+		st_h->update();
 
 		l->add_element(lbl);
 		for(auto &b:buttons){
-			l->add_element(b);
 			b->bind([this, &b](){
 				enter_digit((b->get_label())->get_text()[0]);
 			});
 		}
 		for(auto &b:actions){
-			l->add_element(b);
 		}
+		l->add_element(st_h);
+		st_h->move(0, lbl->get_h() + 1);
+
 		b_plus->bind(std::bind(&myform::add, this));
 		b_minus->bind(std::bind(&myform::sub, this));
 		b_mul->bind(std::bind(&myform::mul, this));
@@ -161,4 +194,5 @@ int main(){
 	watchlib lib("calc");
 	lib.init();
 	lib.set_form(f);
+	lib.start();
 };
