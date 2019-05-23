534196ab514835a5e9eca494ed5f0192fb5d885b
tort_dla_psa <alexeev-eugene@outlook.com>
2019-05-23 16:13:19 +0300

    now it works efficiently

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-git submodule foreach git pull origin master && rm build/*.flag && make all extra="-g"
+updates=$(git submodule foreach git pull origin master | grep "up to date" | wc -l);
+directories=$(ls -l submodules | grep ^d | wc -l);
+count=$(($directories-$updates));
+
+echo  "$count submodules was updated";
+
+if [ $count -ne 0 ]
+then
+ 	rm build/*.flag && make all extra="-g";
+fi
